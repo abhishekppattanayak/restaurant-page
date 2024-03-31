541cfb9095c89601eb553ed6c512baebefc10e7b
Abhishek Pattanyak <2021.abhishek.pattanayak@ves.ac.in>
2024-03-31 23:57:46 +0530

    Add tailwindcss config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,11 +8,35 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 20,
+        poll: 100,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'postcss-loader',
+                ],
+            },
+        ],
+    },
     devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
             title: 'Restaurant Page',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
         }),
     ]
 };
